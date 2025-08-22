64ce916b0f309a55179d32c60adc2dfa2f9400dd
bmouhib <bmouhib@student.42.fr>
2025-08-22 15:17:25 +0200

    Added test

--- 07/ex01/main.cpp
+++ 07/ex01/main.cpp
@@ -5,16 +5,24 @@ void	print(const char a)
 	std::cout << a << std::endl;
 }
 
-void print_next(int a)
+void add_one(int& a)
 {
-	std::cout << a + 1 << std::endl;
+	a++;
+}
+void print_minus_one(int a)
+{
+	std::cout << a - 1 << std::endl;
 }
 
 int main( void ) {
 	int array[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	const char string[4] = {'w', 'o', 'r', 'd'};
 
-	::iter(array, 10, print_next);
+	::iter(array, 10, print_minus_one);
+	::iter(array, 10, add_one);
+	for (int i =0; i < 10; i++)
+		std::cout << array[i] << " ";
+	std::cout << std::endl;
 	::iter(string, 4, print);
 	return 0;
 }
